91dce60298a7de956e787e5119e806737b7e3043
Jairo Abraham Bernal Villanueva <jabernal@gmail.com>
2025-07-08 11:26:39 -0500

    Updates

---
+++ assets/HomeView-B03W_4iA.js
@@ -0,0 +1,31 @@
+import{I as C,O as x,c as u,e,x as m,K as d,B as c,F as y,i as k,d as s,k as p,P as M,t as o,p as f,A as _,q as g,j as w}from"./vue-vendor-WgFWCJB4.js";import{d as V,e as j,f as I}from"./primevue-B6wQC-LW.js";import{c as l,t as q,C as z,_ as B}from"./index-C6aQUCjb.js";/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const T=l("calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]);/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=l("file-pen",[["path",{d:"M12.5 22H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v9.5",key:"1couwa"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M13.378 15.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z",key:"1y4qbx"}]]);/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=l("key",[["path",{d:"m15.5 7.5 2.3 2.3a1 1 0 0 0 1.4 0l2.1-2.1a1 1 0 0 0 0-1.4L19 4",key:"g0fldk"}],["path",{d:"m21 2-9.6 9.6",key:"1j0ho8"}],["circle",{cx:"7.5",cy:"15.5",r:"5.5",key:"yqb3hr"}]]);/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=l("palette",[["path",{d:"M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z",key:"e79jfc"}],["circle",{cx:"13.5",cy:"6.5",r:".5",fill:"currentColor",key:"1okk4w"}],["circle",{cx:"17.5",cy:"10.5",r:".5",fill:"currentColor",key:"f64h9f"}],["circle",{cx:"6.5",cy:"12.5",r:".5",fill:"currentColor",key:"qy21gx"}],["circle",{cx:"8.5",cy:"7.5",r:".5",fill:"currentColor",key:"fotxhn"}]]);/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=l("shield-check",[["path",{d:"M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z",key:"oel41y"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-vue-next v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=l("table",[["path",{d:"M12 3v18",key:"108xh3"}],["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M3 9h18",key:"1pudct"}],["path",{d:"M3 15h18",key:"5xshup"}]]),S={class:"min-h-screen bg-surface-ground py-6"},$={class:"container mx-auto px-4 sm:px-6 lg:px-8"},D={class:"mb-16"},E={class:"flex items-center"},K={class:"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"},O={class:"text-surface-700 text-sm"},A=C({__name:"Index",setup(v){const h={Codificación:z,Conversión:P,Colores:F,Generadores:N,Seguridad:H,Texto:L,Utilidades:T},i=x(()=>{const n={};return q.forEach(t=>{n[t.category]||(n[t.category]=[]),n[t.category].push(t)}),n}),b=x(()=>Object.keys(i.value).sort());return(n,t)=>(s(),u("div",S,[e("div",$,[t[1]||(t[1]=e("header",{class:"text-center mb-8"},[e("h1",{class:"text-4xl font-extrabold text-primary-700 mb-2 flex items-center justify-center"}," 🛠️ Caja de herramientas Web "),e("p",{class:"text-xl text-surface-600"},"Juegos web")],-1)),e("main",D,[m(d(V),{class:"max-w-6xl mx-auto"},{default:c(()=>[(s(!0),u(y,null,k(b.value,r=>(s(),p(d(j),{key:r},{header:c(()=>[e("div",E,[h[r]?(s(),p(g(h[r]),{key:0,class:"w-5 h-5 mr-3 text-primary-600"})):w("",!0),e("span",null,o(r)+" ("+o(i.value[r].length)+")",1)])]),default:c(()=>[e("div",K,[(s(!0),u(y,null,k(i.value[r],a=>(s(),p(d(I),{key:a.route},{legend:c(()=>[(s(),p(g(a.icon),{class:f(["w-6 h-6 mr-3 flex-shrink-0",a.iconColor])},null,8,["class"])),_(" "+o(a.label),1)]),default:c(()=>[m(d(M),{to:a.route,class:"block p-6"},{default:c(()=>[e("p",O,o(a.description),1),e("div",{class:f(["mt-4 font-medium flex items-center",a.linkColor])},[_(o(a.linkText)+" ",1),t[0]||(t[0]=e("i",{class:"pi pi-arrow-right ml-1"},null,-1))],2)]),_:2},1032,["to"])]),_:2},1024))),128))])]),_:2},1024))),128))]),_:1})])])]))}}),G=B(A,[["__scopeId","data-v-c597b67f"]]),W=C({__name:"HomeView",setup(v){return(h,i)=>(s(),u("main",null,[m(G)]))}});export{W as default};
